ba80c9a50716a40d31a7a391a34f36d0691a67fd
Aleksandr Semenov <saney00rus@gmail.com>
2023-01-03 21:18:22 +0300

    add POST

--- carstore/store/views.py
+++ carstore/store/views.py
@@ -11,7 +11,22 @@ class CarstoreAPIView(APIView):
         c = ad_car.objects.all()
         return Response({'posts': CarstoreSerializer(c, many=True).data})
 
+    def post(self, request):
+        serializer = CarstoreSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
 
+        post_new = ad_car.objects.create(
+            mark=request.data['mark'],
+            mod=request.data['mod'],
+            year=request.data['year'],
+            millage=request.data['millage'],
+            owners=request.data['owners'],
+            price=request.data['price'],
+            description=request.data['description'],
+            user=request.data['user']
+        )
+
+        return Response({'post': CarstoreSerializer(post_new).data})
 
 # class CarstoreViewSet(ModelViewSet):
 #     queryset = ad_car.objects.all()
