459e9467b2959c01459f2bdf438f711bc7185df1
Aleksandr Semenov <saney00rus@gmail.com>
2023-01-03 20:44:39 +0300

    CHANGE API

--- carstore/store/serializers.py
+++ carstore/store/serializers.py
@@ -1,11 +1,22 @@
 from rest_framework.fields import ReadOnlyField
 from rest_framework.serializers import ModelSerializer
 from store.models import ad_car
+from rest_framework import serializers
 
 
-class CarstoreSerializer(ModelSerializer):
-    user = ReadOnlyField(source='user.username')
-    class Meta:
-        model = ad_car
-        fields = '__all__'
+# class CarstoreSerializer(ModelSerializer):
+#     user = ReadOnlyField(source='user.username')
+#     class Meta:
+#         model = ad_car
+#         fields = '__all__'
 
+class CarstoreSerializer(serializers.Serializer):
+    mark = serializers.CharField(max_length=255)
+    mod = serializers.CharField(max_length=255)
+    year = serializers.IntegerField()
+    millage = serializers.IntegerField()
+    owners = serializers.IntegerField()
+    images = serializers.ImageField(null=True, blank=True)
+    price = serializers.IntegerField()
+    description = serializers.CharField()
+    user = serializers.IntegerField()
--- carstore/store/views.py
+++ carstore/store/views.py
@@ -1,9 +1,18 @@
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
 
 from store.models import ad_car
 from store.serializers import CarstoreSerializer
 
 
-class CarstoreViewSet(ModelViewSet):
-    queryset = ad_car.objects.all()
-    serializer_class = CarstoreSerializer
+class CarstoreAPIView(APIView):
+    def get(self, request):
+        c = ad_car.objects.all()
+        return Response({'posts': CarstoreSerializer(c, many=True).data})
+
+
+
+# class CarstoreViewSet(ModelViewSet):
+#     queryset = ad_car.objects.all()
+#     serializer_class = CarstoreSerializer
