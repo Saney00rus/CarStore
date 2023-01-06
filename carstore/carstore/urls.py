b1888dda3a80cac700687b8dec51d5e0927d0a1d
Aleksandr Semenov <saney00rus@gmail.com>
2023-01-06 11:29:00 +0300

    + djoser + JWT token

--- carstore/carstore/urls.py
+++ carstore/carstore/urls.py
@@ -1,14 +1,21 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
+
 from store.views import *
 from .yasg import urlpatterns as swagger
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('auth/', include('rest_framework.urls')),
+    path('api/drf-auth/', include('rest_framework.urls')),
     path('api/cars/', CarstoreAPIList.as_view()),
     path('api/cars/<int:pk>/', CarstoreAPIUpdateAndDestroy.as_view()),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify', TokenVerifyView.as_view(), name='token_verify'),
+    path('api/auth/', include('djoser.urls')),
+    re_path(r'^auth/', include('djoser.urls.authtoken')),
 ]
 
 urlpatterns += swagger
